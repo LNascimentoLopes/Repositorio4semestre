2f011775f4e20f2ebbf357fc64f6bc838ee2768d
LNascimentoLopes <lucas.nascimento3224@gmail.com>
2026-08-13 21:02:09 -0300

    first commit

---
+++ imagem_pecas/src/test/java/com/soloco/imagem_pecas/ImagemPecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soloco.imagem_pecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagemPecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
